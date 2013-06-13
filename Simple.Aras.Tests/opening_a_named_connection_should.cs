20146a414906a7b2ac2b94b3f04aae2564891f1b
thefringeninja <joao.paul@braganca.name>
2013-06-13 16:36:16 -0700

    Making connections configurable

---
+++ Simple.Aras.Tests/opening_a_named_connection_should.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+using Aras.IOM;
+using Moq;
+using NUnit.Framework;
+using Should.Fluent;
+
+namespace Simple.Aras.Tests
+{
+    // ReSharper disable InconsistentNaming
+    public class opening_a_named_connection_should : ActionSpecification<dynamic>
+// ReSharper restore InconsistentNaming
+    {
+        protected override void Setup()
+        {
+        }
+
+        protected override dynamic Given()
+        {
+            return ArasInnovator.OpenConnection(new Uri("http://user:password@localhost/InnovatorServer;database=development"));
+        }
+
+        [Test]
+        public void return_an_aras_innovator_adapter()
+        {
+            ((object) Sut).Should().Be.OfType<ArasInnovatorAdaptor>();
+        }
+    }
+}
--- Simple.Aras/ArasInnovator.cs
+++ Simple.Aras/ArasInnovator.cs
@@ -1,12 +1,48 @@
-﻿using Aras.IOM;
+﻿using System;
+using System.Configuration;
+using Aras.IOM;
 
 namespace Simple.Aras
 {
     public static class ArasInnovator
     {
+        private const string DatabasePrefix = "database=";
+
         public static dynamic Open(IServerConnection connection)
         {
             return new ArasInnovatorAdaptor(IomFactory.CreateInnovator(connection));
+        }
+
+        public static dynamic OpenNamedConnection(string connectionStringName)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[connectionStringName];
+            if (connectionString == null) throw new ArgumentOutOfRangeException("connectionStringName");
+
+            return OpenConnection(connectionString.ConnectionString);
+        }
+
+        public static dynamic OpenConnection(string connection)
+        {
+            return OpenConnection(new Uri(connection));
+        }
+
+        public static dynamic OpenConnection(Uri connection)
+        {
+            if (connection == null) throw new ArgumentNullException("connection");
+
+            var builder = new UriBuilder(connection);
+            var pathParts = builder.Path.Split(';');
+            if (pathParts.Length != 2 || false == pathParts[1].StartsWith(DatabasePrefix)) throw new ArgumentException("Expected /{Path};database={database}, received " + builder.Path + " instead.");
+
+            var userName = builder.UserName;
+            var password = builder.Password;
+            var database = pathParts[1].Remove(0, DatabasePrefix.Length);
+
+            builder.Path = pathParts[0];
+            builder.UserName = builder.Password = String.Empty;
+            
+            var httpServerConnection = IomFactory.CreateHttpServerConnection(builder.Uri.ToString(), database, userName, password);
+            return new ArasInnovatorAdaptor(new Innovator(httpServerConnection));
         }
     }
 }
